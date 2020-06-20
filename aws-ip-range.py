66a86c18b8f8f110af94ce70d8ca27e7e03b1b0e
Gustavo Aguiar <gustavo.aguiar@gmail.com>
2020-06-20 08:12:42 -0400

    Implemented get_all_data get_region get_service

--- aws-ip-range.py
+++ aws-ip-range.py
@@ -25,19 +25,30 @@ def create_response(status_code, message_content, message_key='key'):
 # download ip-range file
 def get_ip_range_json():
     logger.debug("Inside function: [%s]", inspect.currentframe().f_code.co_name)
-
     response = urllib.request.urlopen(os.environ['IP_RANGE_JSON_URL'])
     json_data = json.loads(response.read())
-
     return json_data
 
-# handler
-def get_region(event, context):
+# return all data
+def get_all_data(event, context):
     logger.debug("Inside function: [%s]", inspect.currentframe().f_code.co_name)
-
     ip = event['pathParameters']['ip']
     logger.debug("IP: [%s]", ip)
+    data = 'unknown'
+    ip_data = get_ip_range_json()
+    prefixes = ip_data['prefixes']
+    my_ip = ip_address(ip)
+    for prefix in prefixes:
+        if my_ip in ip_network(prefix['ip_prefix']):
+            data = prefix
+            # break
+    return create_response(200, data, 'data')
 
+# return region
+def get_region(event, context):
+    logger.debug("Inside function: [%s]", inspect.currentframe().f_code.co_name)
+    ip = event['pathParameters']['ip']
+    logger.debug("IP: [%s]", ip)
     region = 'unknown'
     ip_data = get_ip_range_json()
     prefixes = ip_data['prefixes']
@@ -45,6 +56,22 @@ def get_region(event, context):
     for prefix in prefixes:
         if my_ip in ip_network(prefix['ip_prefix']):
             region = prefix['region']
-            return create_response(200, region, 'region')
+            # break
+    return create_response(200, region, 'region')
+
+# return service
+def get_service(event, context):
+    logger.debug("Inside function: [%s]", inspect.currentframe().f_code.co_name)
+    ip = event['pathParameters']['ip']
+    logger.debug("IP: [%s]", ip)
+    service = 'unknown'
+    ip_data = get_ip_range_json()
+    prefixes = ip_data['prefixes']
+    my_ip = ip_address(ip)
+    for prefix in prefixes:
+        if my_ip in ip_network(prefix['ip_prefix']):
+            service = prefix['service']
+            # break
+    return create_response(200, service, 'service')
 
 # End;
